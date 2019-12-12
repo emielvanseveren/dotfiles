76b656135dce1b5251ea99270521e9a7c30b353e
Emiel Van Severen <emielvanseveren@outlook.com>
2019-12-12 11:21:00 +0100

    Add function get the operating system

---
+++ tools/get_os.sh
@@ -0,0 +1,17 @@
+# Echo's the operating system, but simplified to
+# - osx
+# - ubuntu
+
+get_os() {
+	# Identify the operating system.
+	local un=$(uname -a)
+	os="unknown"
+	if [[ "$un" =~ [Dd]arwin ]]; then
+		echo "osx"
+	elif [[ "$un" =~ [Uu]buntu ]]; then
+		echo "ubuntu"
+	else
+		logger -s "Unable to identify operating system from uname '$un'"
+		exit 1
+	fi
+}
