66f9a5f682833d26c8035de17565b6d3fbf4024f
Emiel Van Severen <emielvanseveren@outlook.com>
2020-01-30 00:51:55 +0100

    Add to do

--- vim/setup_vim.sh
+++ vim/setup_vim.sh
@@ -8,6 +8,15 @@ done
 
 echo "Checking if Vim is installed.."
 if [[ ! "$(command -v vim)" ]]; then
+  if [[ ! "$(command -v yarn)" ]]; then
+    curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add - 
+    echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+    sudo apt update
+    sudo apt install yarn 
+  else 
+    echo "Yarn is already installed.."
+  fi    
+
   sudo apt update
   sudo apt install vim -y
   ensure_symlink "$(pwd)/vimrc" "$HOME/.vimrc"
@@ -16,6 +25,9 @@ if [[ ! "$(command -v vim)" ]]; then
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
   vim +PlugInstall
   vim -c "source $HOME/.vimrc"
+
+
+  # TODO: coc symlink
 
 else
   echo "Vim is already installed.."
