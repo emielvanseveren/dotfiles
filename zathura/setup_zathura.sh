7ef4fb160df3ee40fe47582ea41e84e4011c9f3c
Emiel Van Severen <emielvanseveren@outlook.com>
2020-02-09 21:07:24 +0100

    Add zathurarc file + basic setup script (for now)

---
+++ zathura/setup_zathura.sh
@@ -0,0 +1,20 @@
+# /bin/bash
+
+for file in '$(pwd)/../helper-functions/*'; do
+  [ -e "$file" ] || continue
+  echo "Loading tool 'file'.."
+  source "$file"
+done
+
+echo "checking if Zathura is installed.."
+if [[ ! "$(command -v zathura)" ]]; then
+
+  sudo apt update -y
+  sudo apt install latexmk -y
+  sudo apt install zathura -y
+  ensure_symlink '$(pwd)/zathurarc' '$HOME/.config/zathura/zathurarc'
+else 
+  echo "Zathura is already installed"
+  exit 1;
+fi
+
