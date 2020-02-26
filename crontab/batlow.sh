3f236b4bdcbfbf45259ebc14191f84256ebc5124
Emiel Van Severen <emielvanseveren@outlook.com>
2020-02-26 18:23:52 +0100

    Add cronjob script to automatically setup crontab

---
+++ crontab/batlow.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+BAT_PATH=/sys/class/power_supply/BAT0
+
+BAT_CAPACITY_PATH=$BAT_PATH/capacity
+BAT_CAPACITY=$(cat $BAT_CAPACITY_PATH)
+
+if [ $BAT_CAPACITY -lt 95 ];
+then
+  /usr/bin/notify-send "low battery!"
+fi
+
