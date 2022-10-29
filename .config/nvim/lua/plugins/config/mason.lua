6cbc4340e8026675ccf8c80493d90d111d7da813
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2022-10-29 16:04:35 +0200

    Feat: make sure texlab lsp is installed

--- .config/nvim/lua/plugins/config/mason.lua
+++ .config/nvim/lua/plugins/config/mason.lua
@@ -3,5 +3,13 @@ require("mason").setup()
 
 -- This installs the actual language servers
 require("mason-lspconfig").setup({
-  ensure_installed = { "sumneko_lua", "tsserver", "yamlls", "pyright", "eslint", "jsonls" }
+	ensure_installed = {
+		"sumneko_lua",
+		"tsserver",
+		"yamlls",
+		"pyright",
+		"eslint",
+		"jsonls",
+		"texlab",
+	},
 })
