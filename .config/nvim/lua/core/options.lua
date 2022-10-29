673b672695c5ed2ea5353113bb53efb788239cce
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2022-10-29 16:18:07 +0200

    Chore: remove options that use the default values + remove old options

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -6,36 +6,36 @@ g.mapleader = "," -- Sets leader key to 'COMMA'
 
 g.vim_version = vim.version().minor
 
-opt.shell          = "/usr/bin/zsh" -- Sets default shell to zsh
-opt.compatible     = false -- don't try to be like vi.
-opt.completeopt    = "menuone,noinsert,noselect"
-opt.cmdheight      = 1 -- Height of command line
-opt.updatetime     = 750 -- milliseconds nothing is typed the swap file will be written to disk
-opt.termguicolors  = true -- Enable termguicolors
-opt.background     = "dark" -- Try to use dark colors
-opt.clipboard      = "unnamedplus" -- copy to system clipboard on yank
+opt.shell = "/usr/bin/zsh" -- Sets default shell to zsh
+opt.compatible = false -- don't try to be like vi.
+opt.completeopt = "menuone,noinsert,noselect"
+opt.cmdheight = 1 -- Height of command line
+opt.updatetime = 750 -- milliseconds nothing is typed the swap file will be written to disk
+opt.termguicolors = true -- Enable termguicolors
+opt.background = "dark" -- Try to use dark colors
+opt.clipboard = "unnamedplus" -- copy to system clipboard on yank
 opt.relativenumber = true -- Show the line number relative to the line with the cursor
-opt.linebreak      = true -- Break lines when max is reached
-opt.mouse          = "a" -- Enable the mouse in all modes
-opt.scrolloff      = 5 -- Keep 5 lines below and above the cursor
-opt.joinspaces     = false -- Combine >1 consecutive spaces
-opt.wrap           = false -- No line wrapping
-opt.conceallevel   = 0
-opt.autochdir      = true -- Vim will change the current working dir whenever you open a file / switch buffers
-opt.ruler          = true -- Show the line and column number of the curser position
-opt.tabstop        = 2 -- Number of spaces that a <TAB> in the file counts for
-opt.shiftwidth     = 2 -- Tab shift
-opt.softtabstop    = 2 -- 2 space tab
-opt.expandtab      = true -- Use spaces for tabs
-opt.ignorecase     = true -- ignore case when searching
+opt.linebreak = true -- Break lines when max is reached
+opt.mouse = "a" -- Enable the mouse in all modes
+opt.scrolloff = 5 -- Keep 5 lines below and above the cursor
+opt.joinspaces = false -- Combine >1 consecutive spaces
+opt.wrap = false -- No line wrapping
+opt.conceallevel = 0
+opt.autochdir = true -- Vim will change the current working dir whenever you open a file / switch buffers
+opt.ruler = true -- Show the line and column number of the curser position
+opt.tabstop = 2 -- Number of spaces that a <TAB> in the file counts for
+opt.shiftwidth = 2 -- Tab shift
+opt.softtabstop = 2 -- 2 space tab
+opt.expandtab = true -- Use spaces for tabs
+opt.ignorecase = true -- ignore case when searching
 
 -- ===============================================================
 -- # Gui Settings
 -- ===============================================================
-opt.backspace    = "2" -- Backspace over newlines
-opt.foldenable   = false -- Disable folding by default
-opt.number       = true -- show line numbers
-opt.cursorline   = true -- highlight current line
+opt.backspace = "2" -- Backspace over newlines
+opt.foldenable = false -- Disable folding by default
+opt.number = true -- show line numbers
+opt.cursorline = true -- highlight current line
 
 -- Show those damn hidden characters
 opt.listchars = "nbsp:¬,eol:¶,extends:»,precedes:«,trail:•,tab:»·"
@@ -50,8 +50,8 @@ api.nvim_command("au FileType yaml setl sw=2 sts=2")
 -- ===============================================================
 -- # Built-in debugger
 -- ===============================================================
-vim.cmd "packadd termdebug" -- Add termdebug
-g.termdebugger   = "rust-gdb" -- Set debugger to rust-gdb
+vim.cmd("packadd termdebug") -- Add termdebug
+g.termdebugger = "rust-gdb" -- Set debugger to rust-gdb
 g.termdebug_wide = 1 -- Set debugger to use wide terminal
 
 -- ===============================================================
@@ -59,21 +59,33 @@ g.termdebug_wide = 1 -- Set debugger to use wide terminal
 -- ===============================================================
 
 -- Github copilot
-g.completion_enable_auto_popup    = 1 -- Auto popup
+g.completion_enable_auto_popup = 1 -- Auto popup
 g.completion_matching_ingore_case = 1 -- While in popup ignore caps.
-g.copilot_no_tab_map              = 1 -- Don't use <Tab> but <C-J> to accept the suggestion.
-g.copilot_node_command            = "~/.nvm/versions/node/v17.9.1/bin/node"
+g.copilot_no_tab_map = 1 -- Don't use <Tab> but <C-J> to accept the suggestion.
+g.copilot_node_command = "~/.nvm/versions/node/v17.9.1/bin/node"
 api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 
 -- Rust fmt
-g.rust_clip_command      = "xclip -selection clipboard"
+g.rust_clip_command = "xclip -selection clipboard"
 
 -- Vimtex (\ll to start compilation)
-g.tex_flavor                   = "latex"
-g.vimtex_view_method           = "zathura" -- Use zathura to view compiled pdfs
+g.tex_flavor = "latex"
+g.vimtex_view_method = "zathura" -- Use zathura to view compiled pdfs
+g.vimtex_compiler_method = "latexmk"
 g.vimtex_complete_close_braces = 1
-g.vimsyn_embed                 = 0 -- allows u to select what, if any, types of embedded script highlighting they wish to have.
+g.vimtex_compiler_latexmk = {
+	options = {
+		"-pdf",
+		"-shell-escape",
+		"-verbose",
+		"-file-line-error",
+		"-synctex=1",
+		"-interaction=nonstopmode",
+	},
+}
+
+g.vimsyn_embed = 0 -- allows u to select what, if any, types of embedded script highlighting they wish to have.
 
 -- Beacon
 g.beacon_minimal_jump = 5 -- Only show beacon if the cursor jumps more than 5 lines.
-g.beacon_size         = 40 -- Size of the beacon
+g.beacon_size = 40 -- Size of the beacon
