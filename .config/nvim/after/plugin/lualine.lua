4c576fc5aed2da2b3d895259b82b78be2ccfa4c5
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2023-02-27 00:45:00 +0100

    Style: 2 spaces

--- .config/nvim/after/plugin/lualine.lua
+++ .config/nvim/after/plugin/lualine.lua
@@ -1,26 +1,22 @@
 local present, lualine = pcall(require, "lualine")
 
-
-
 if not present then
   return
 end
 
 lualine.setup({
   options = {
-    icons_enabled        = true,
-    theme                = "auto",
+    icons_enabled = true,
+    theme = "auto",
     always_divide_middle = true,
     component_separators = { left = "", right = "" },
   },
-
   sections = {
-    lualine_a = { 'mode' },
-    lualine_b = { 'branch', 'diff' },
+    lualine_a = { "mode" },
+    lualine_b = { "branch", "diff" },
 
-
-    lualine_x = { 'filetype' },
-    lualine_y = { 'fileformat' },
-    lualine_z = { 'location' },
-  }
+    lualine_x = { "filetype" },
+    lualine_y = { "fileformat" },
+    lualine_z = { "location" },
+  },
 })
