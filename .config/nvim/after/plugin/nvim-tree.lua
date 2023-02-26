365d918b7275521794e23d59b06f0ffed3b6c342
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2023-02-27 00:46:08 +0100

    Style: 2 spaces

--- .config/nvim/after/plugin/nvim-tree.lua
+++ .config/nvim/after/plugin/nvim-tree.lua
@@ -1,23 +1,23 @@
 local present, nvimtree = pcall(require, "nvim-tree")
 
 if not present then
-	return
+  return
 end
 
 nvimtree.setup({
-	disable_netrw = true,
-	hijack_netrw = true,
-	hijack_unnamed_buffer_when_opening = false,
-	diagnostics = {
-		enable = true,
-	},
-	update_cwd = true,
-	view = {
-		adaptive_size = true,
-	},
-	filters = {
-		dotfiles = true,
-	},
+  disable_netrw = true,
+  hijack_netrw = true,
+  hijack_unnamed_buffer_when_opening = false,
+  diagnostics = {
+    enable = true,
+  },
+  update_cwd = true,
+  view = {
+    adaptive_size = true,
+  },
+  filters = {
+    dotfiles = true,
+  },
 })
 
 vim.keymap.set("n", "<leader><leader>t", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer treeview." })
