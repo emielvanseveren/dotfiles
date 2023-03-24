46e3371475be7c84fbcc10a6caae8d80e955a903
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2023-03-24 01:31:53 +0100

    Fix: update icons while typing

--- .config/nvim/after/plugin/bufferline.lua
+++ .config/nvim/after/plugin/bufferline.lua
@@ -1,21 +1,21 @@
 local bufferline = require("bufferline")
 
 bufferline.setup({
-	options = {
-		mode = "buffers",
-		separator_style = "slant", -- Space between tabs
-		enforce_regular_tabs = true, -- Allow to extend tab size when name is longer than tab_size
-		show_buffer_default_icon = true, -- Show file icon
-		show_buffer_close_icons = false, -- Show close icon
-		always_show_bufferline = true, -- Only show bufferline when more than 1 tab
-		diagnostics = "nvim_lsp", -- Change color of filename when file has an error
-		diagnostics_update_in_insert = false,
-		numbers = "none", -- Don't show numbers on tab
-		diagnostics_indicator = function(count, level)
-			local icon = level:match("error") and " " or " "
-			return " " .. icon .. count
-		end,
-		offsets = { { filetype = "NvimTree", text = "File explorer", text_align = "left" } },
-		show_buffer_icons = true,
-	},
+  options = {
+    mode = "buffers",
+    separator_style = "slant", -- Space between tabs
+    enforce_regular_tabs = true, -- Allow to extend tab size when name is longer than tab_size
+    show_buffer_default_icon = true, -- Show file icon
+    show_buffer_close_icons = false, -- Show close icon
+    always_show_bufferline = true, -- Only show bufferline when more than 1 tab
+    diagnostics = "nvim_lsp", -- Change color of filename when file has an error
+    diagnostics_update_in_insert = true,
+    numbers = "none", -- Don't show numbers on tab
+    diagnostics_indicator = function(count, level)
+      local icon = level:match("error") and " " or " "
+      return " " .. icon .. count
+    end,
+    offsets = { { filetype = "NvimTree", text = "File explorer", text_align = "left" } },
+    show_buffer_icons = true,
+  },
 })
