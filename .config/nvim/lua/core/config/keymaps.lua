de1604869cb51ac37f5d6eb6b037fb973d06d477
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2023-04-11 22:51:20 +0200

    Add keymaps for lazygit and floating terminal

--- .config/nvim/lua/core/config/keymaps.lua
+++ .config/nvim/lua/core/config/keymaps.lua
@@ -1,4 +1,5 @@
 local map = vim.keymap.set
+local Util = require("lazyvim.util")
 
 map("n", "<leader>Y", ":%y+ <CR>", { desc = "Yank entire file" })
 map("n", "<Esc>", "<cmd> nohlsearch <CR>", { desc = "Remove search highlight" })
@@ -6,12 +7,12 @@ map("n", "<C-s>", "<cmd> w <CR>", { desc = "Save file" })
 map("n", "<F1>", "<NOP>") -- I can type :help on my own, thanks :)
 
 -- Move lines up and down
-map("n", "<leader-j>", "<cmd>m .+1<CR>==", { desc = "Move down" })
-map("n", "<leader-k>", "<cmd>m .-2<CR>==", { desc = "Move up" })
-map("i", "<leader-j>", "<Esc><cmd> m .+1<CR>==gi", { desc = "Move down" })
-map("i", "<leader-k>", "<Esc><cmd> m .-2<CR>==gi", { desc = "Move up" })
-map("v", "<leader-k>", "<cmd>m '<-2<cr>gv=gv", { desc = "Move up" })
-map("v", "<leader-j>", "<cmd>m '>+1<cr>gv=gv", { desc = "Move down" })
+map("n", "<leader><leader>j", "<cmd>m .+1<cr>==", { desc = "Move down" })
+map("n", "<leader><leader>k", "<cmd>m .-2<cr>==", { desc = "Move up" })
+map("i", "<leader><leader>j", "<esc><cmd>m .+1<cr>==gi", { desc = "Move down" })
+map("i", "<leader><leader>k", "<esc><cmd>m .-2<cr>==gi", { desc = "Move up" })
+map("v", "<leader><leader>j", ":m '>+1<cr>gv=gv", { desc = "Move down" })
+map("v", "<leader><leader>k", ":m '<-2<cr>gv=gv", { desc = "Move up" })
 
 -- https://github.com/mhinz/vim-galore#saner-behavior-of-n-and-n
 map("n", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result" })
@@ -20,8 +21,8 @@ map("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result
 map("n", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
 map("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
 map("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
--- I don't like joining two lines
 
+-- I don't like joining two lines
 map("n", "J", "<nop>")
 
 -- Add undo break-points
@@ -57,6 +58,23 @@ map("o", "n", "'Nn'[v:searchforward]", { expr = true, desc = "Next search result
 map("n", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
 map("x", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
 map("o", "N", "'nN'[v:searchforward]", { expr = true, desc = "Prev search result" })
+
+-- floating terminal
+map("n", "<leader>ft", function()
+  Util.float_term(nil, { cwd = Util.get_root() })
+end, { desc = "Terminal (root dir)" })
+map("n", "<leader>fT", function()
+  Util.float_term()
+end, { desc = "Terminal (cwd)" })
+map("t", "<esc>", "<c-\\><c-n>", { desc = "Enter Normal Mode" })
+
+-- lazygit
+map("n", "<leader>gg", function()
+  Util.float_term({ "lazygit" }, { cwd = Util.get_root() })
+end, { desc = "Lazygit (root dir)" })
+map("n", "<leader>gG", function()
+  Util.float_term({ "lazygit" })
+end, { desc = "Lazygit (cwd)" })
 
 -- Move normally by line.
 -- Allow moving the cursor through wrapped lines with j, k, <Up> and <Down>
