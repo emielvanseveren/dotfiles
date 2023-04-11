f9466cea31216515a4caafd67c4d93deb084d392
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2023-04-11 22:21:44 +0200

    Fix: use opts for gitsigns and add correct load events

--- .config/nvim/lua/core/plugins/editor.lua
+++ .config/nvim/lua/core/plugins/editor.lua
@@ -41,39 +41,43 @@ return {
 
   {
     "lewis6991/gitsigns.nvim",
-    event = "VeryLazy",
-    config = function()
-      local gitsigns = require("gitsigns")
-      gitsigns.setup({
-        signs = {
-          add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
-          change = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-          delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-          topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
-          changedelete = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
-        },
-        signcolumn = true, -- show signs in column
-        current_line_blame = true,
-        current_line_blame_opts = {
-          virt_text = true,
-          virt_text_pos = "eol",
-          delay = 1000,
-          ignore_whitespace = false,
-        },
-        linehl = false,
-        numhl = false, -- highlight line number
-        word_diff = false, -- highlight word diffs
-        attach_to_untracked = true,
-        watch_gitdir = {
-          interval = 1000,
-          follow_files = true,
-        },
-      })
-      vim.keymap.set("n", "gj", gitsigns.next_hunk, { desc = "Next git hunk" })
-      vim.keymap.set("n", "gk", gitsigns.prev_hunk, { desc = "Previous git hunk" })
-      vim.keymap.set("n", "gl", gitsigns.blame_line, { desc = "View git blame" })
-      vim.keymap.set("n", "gp", gitsigns.preview_hunk, { desc = "Preview Git hunk" })
-    end,
+    event = { "BufReadPre", "BufNewFile" },
+    opts = {
+      signs = {
+        add = { hl = "GitSignsAdd", text = "│", numhl = "GitSignsAddNr", linehl = "GitSignsAddLn" },
+        change = { hl = "GitSignsChange", text = "│", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+        delete = { hl = "GitSignsDelete", text = "_", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+        topdelete = { hl = "GitSignsDelete", text = "‾", numhl = "GitSignsDeleteNr", linehl = "GitSignsDeleteLn" },
+        changedelete = { hl = "GitSignsChange", text = "~", numhl = "GitSignsChangeNr", linehl = "GitSignsChangeLn" },
+      },
+      signcolumn = true, -- show signs in column
+      current_line_blame = true,
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol",
+        delay = 1000,
+        ignore_whitespace = false,
+      },
+      linehl = false,
+      numhl = false, -- highlight line number
+      word_diff = false, -- highlight word diffs
+      attach_to_untracked = true,
+      watch_gitdir = {
+        interval = 1000,
+        follow_files = true,
+      },
+      on_attach = function(buffer)
+        local gs = package.loaded.gitsigns
+
+        local function map(mode, l, r, desc)
+          vim.keymap.set(mode, l, r, { buffer = buffer, desc = desc })
+        end
+        map("n", "gj", gs.next_hunk, "Next git hunk")
+        map("n", "gk", gs.prev_hunk, "Previous git hunk")
+        map("n", "gl", gs.blame_line, "View git blame")
+        map("n", "gp", gs.preview_hunk, "Preview Git hunk")
+      end,
+    },
   },
 
   {
@@ -98,6 +102,7 @@ return {
     "nvim-telescope/telescope.nvim",
     version = false, -- telescope did only one release, so use HEAD for now
     dependencies = { "nvim-lua/plenary.nvim" },
+    cmd = "Telescope",
     keys = {
       -- search
       { "fg", Util.telescope("live_grep"), desc = "Find in Files (Grep)" },
