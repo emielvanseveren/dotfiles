894f7899554299c35ecae04ffd4235703826a84e
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2023-04-11 22:20:47 +0200

    Feat: use lazyvims treesitter config

--- .config/nvim/lua/core/plugins/treesitter.lua
+++ .config/nvim/lua/core/plugins/treesitter.lua
@@ -1,52 +1,54 @@
 return {
-  "nvim-treesitter/nvim-treesitter",
-  build = ":TSUpdate",
-  event = { "BufReadPost", "BufNewFile" },
-  version = false, -- last version is super old
-  dependencies = { "nvim-treesitter/nvim-treesitter-textobjects", { "nvim-treesitter/nvim-treesitter-context", opts = {} } },
-  opts = {
-    -- ensure_installed = "all", -- one of "all", "maintained" (parsers with maintainers), or a list of languages
-    sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-    auto_install = true, -- automatically install missing parsers when entering a buffer.
-    indent = { enable = true },
-    highlight = {
-      enable = true, -- false will disable the whole extension
-      disable = { "latex" }, -- list of language that will be disabled
-      -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
-      -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
-      -- Using this option may slow down your editor, and you may see some duplicate highlights.
-      -- Instead of true it can also be a list of languages
-      additional_vim_regex_highlighting = false,
+  {
+    "nvim-treesitter/nvim-treesitter",
+    version = false, -- last release is way too old and doesn't work on Windows
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    dependencies = {
+      {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        init = function()
+          -- PERF: no need to load the plugin, if we only need its queries for mini.ai
+          local plugin = require("lazy.core.config").spec.plugins["nvim-treesitter"]
+          local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+          local enabled = false
+          if opts.textobjects then
+            for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
+              if opts.textobjects[mod] and opts.textobjects[mod].enable then
+                enabled = true
+                break
+              end
+            end
+          end
+          if not enabled then
+            require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
+          end
+        end,
+      },
     },
-    matchup = {
-      enable = true,
+    keys = {
+      { "<leader>space>", desc = "Increment selection" },
+      { "<bs>", desc = "Decrement selection", mode = "x" },
     },
-    textobjects = {
-      select = {
+    ---@type TSConfig
+    opts = {
+      highlight = { enable = true },
+      indent = { enable = true },
+      context_commentstring = { enable = true, enable_autocmd = false },
+      ensure_installed = "all",
+      incremental_selection = {
         enable = true,
-        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
         keymaps = {
-          ["af"] = "@function.outer", -- select from current position until end of function
-          ["if"] = "@function.inner", -- select from current position until end of fuction but without
-        },
-      },
-      swap = {
-        enable = true,
-        swap_next = {
-          ["<leader>a"] = "@parameter.inner", -- swap with item underneed
-        },
-        swap_previous = {
-          ["<leader>A"] = "@parameter.inner", -- swap with item above
+          init_selection = "<leader><Space>",
+          node_incremental = "<leader><Space>",
+          scope_incremental = "<nop>",
+          node_decremental = "<bs>",
         },
       },
     },
-    rainbow = {
-      enable = true,
-      extended_mode = true,
-      max_file_lines = nil,
-    },
-    autotag = {
-      enable = true,
-    },
+    ---@param opts TSConfig
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+    end,
   },
 }
