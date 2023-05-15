9044945aab1a3c15779998102f64b86e2c6ee5a3
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2023-05-15 14:37:24 +0200

    Feat: add better keybinds for treesitter

--- .config/nvim/lua/core/plugins/treesitter.lua
+++ .config/nvim/lua/core/plugins/treesitter.lua
@@ -4,30 +4,8 @@ return {
     version = false, -- last release is way too old and doesn't work on Windows
     build = ":TSUpdate",
     event = { "BufReadPost", "BufNewFile" },
-    dependencies = {
-      {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-        init = function()
-          -- PERF: no need to load the plugin, if we only need its queries for mini.ai
-          local plugin = require("lazy.core.config").spec.plugins["nvim-treesitter"]
-          local opts = require("lazy.core.plugin").values(plugin, "opts", false)
-          local enabled = false
-          if opts.textobjects then
-            for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
-              if opts.textobjects[mod] and opts.textobjects[mod].enable then
-                enabled = true
-                break
-              end
-            end
-          end
-          if not enabled then
-            require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
-          end
-        end,
-      },
-    },
     keys = {
-      { "<leader>space>", desc = "Increment selection" },
+      { "<leader>s", desc = "Increment selection" },
       { "<bs>", desc = "Decrement selection", mode = "x" },
     },
     ---@type TSConfig
@@ -39,9 +17,9 @@ return {
       incremental_selection = {
         enable = true,
         keymaps = {
-          init_selection = "<leader><Space>",
-          node_incremental = "<leader><Space>",
-          scope_incremental = "<nop>",
+          init_selection = "<leader>s",
+          node_incremental = "<leader>s",
+          scope_incremental = false,
           node_decremental = "<bs>",
         },
       },
