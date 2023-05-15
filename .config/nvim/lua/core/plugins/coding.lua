c7855fb4f14a111065252ddc2f192f937a821d8a
Emiel Van Severen <35073890+emielvanseveren@users.noreply.github.com>
2023-05-15 14:33:36 +0200

    Feat: add extra ansible extension for filetype detection

--- .config/nvim/lua/core/plugins/coding.lua
+++ .config/nvim/lua/core/plugins/coding.lua
@@ -42,8 +42,8 @@ return {
     lazy = "vimEnter",
     init = function()
       -- Github copilot
-      vim.g.completion_enable_auto_popup = 1 -- Auto popup
-      vim.g.copilot_no_tab_map = 1 -- Don't use <Tab> but <C-J> to accept the suggestion. Tab is already used for cmp.
+      vim.g.completion_enable_auto_popup = 1    -- Auto popup
+      vim.g.copilot_no_tab_map = 1              -- Don't use <Tab> but <C-J> to accept the suggestion. Tab is already used for cmp.
       vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
       vim.g.completion_matching_ingore_case = 1 -- While in popup ignore caps.
     end,
@@ -52,6 +52,11 @@ return {
   {
     "fatih/vim-go",
     build = ":GoInstallBinaries",
+  },
+
+  {
+    "mfussenegger/nvim-ansible",
+    ft = "yaml",
   },
 
   -- git
