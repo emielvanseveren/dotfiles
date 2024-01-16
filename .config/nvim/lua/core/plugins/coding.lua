a86cfae4385bc6b36feea3406f64d688b713db04
Emiel Van Severen <github@vanseveren.dev>
2024-01-16 09:38:05 +0100

    Fix: use zathura_simple which supports wayland

--- .config/nvim/lua/core/plugins/coding.lua
+++ .config/nvim/lua/core/plugins/coding.lua
@@ -6,7 +6,7 @@ return {
     ft = "tex",
     init = function()
       vim.g.vimtex_compiler_method = "latexmk"
-      vim.g.vimtex_view_method = "zathura"
+      vim.g.vimtex_view_method = "zathura_simple" -- basic version of wayland support
       vim.g.vimtex_complete_close_braces = 1
       vim.g.vimtex_compiler_latexmk = {
         options = {
